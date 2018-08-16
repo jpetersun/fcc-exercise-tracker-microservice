43fbbfa39c83b770ebb0597797d26ebfcca8f85f
James Peterson <petersonj020@gmail.com>
2018-08-16 09:54:41 -0700

    chore: add api comments, and ignore .env file

--- index.js
+++ index.js
@@ -11,11 +11,19 @@ app.get('/', (req, res) => {
 })
 
 // 1. POST /api/exercise/new-user
-// 2. POST /api/exercise/add
+// request: { username: String }
+// response: { username, _id }
+
+// 2. POST /api/exercise/add?{userId}
+// request: { _id: String, description: String, duration: Number, date: String(YYYY-MM-DD) }
+// response: { _id, username, description, duration,  date }
+
 // 3. GET /api/exercise/log?{userId}[&from][&to][&limit]
+// request: { userId: String, from: String(YYYY-MM-DD), to: String(YYYY-MM-DD), limit: Number }
 // {} = required, [] = optional
 // from, to = (YYYY-MM-DD)
 // limit = Number
+// response: { userId, from, to, log: Array()}
 
 // error handler middleware using boom
 app.use((err, req, res, next) => {
